c7809d6078e7cdd4ae9b328a51aa8a951b18cf31
F4463466 Italo Franchesco Magalhaes Feitosa <italo.feitosa@bb.com.br>
2022-08-05 13:09:11 -0300

    Criado projeto utilizando spring

---
+++ src/main/java/br/com/letscode/supernova/batatas/BatatasApplication.java
@@ -0,0 +1,13 @@
+package br.com.letscode.supernova.batatas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatatasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatatasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/letscode/supernova/batatas/BatatasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.letscode.supernova.batatas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatatasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
